b5034419dfb36244bd329dcd4b734ab681caa8e3
Hanifa Arrumaisha <hanifa.arrumaisha@ecomindo.com>
2021-07-13 15:04:57 +0700

    add sync code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,73 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Coffee cup = PourCoffee();
+
         }
+
+        private static Juice pourOJ()
+        {
+            Console.WriteLine("Pouring Orange Juice...");
+            return new Juice;
+        }
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on toast...");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Apply butter on toast...");
+
+        private static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster...")
+            }
+            Console.WriteLine("Start Toasting");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove Toast from the Toaster");
+
+            return new Toast();
+        }
+
+        private static Bacon FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            Task.Delay(3000).Wait();
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private static Egg FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
     }
+
+    class Coffee { }
+    class Juice { }
+    class Toast { }
+    class Bacon { }
+    class Egg { }
 }
